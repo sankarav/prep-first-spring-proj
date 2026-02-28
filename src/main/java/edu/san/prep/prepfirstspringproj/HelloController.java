b9d34ba201d9d632fdca1ec2dd5c6058e90ba9d1
Sankar D <san.gct@gmail.com>
2026-02-28 14:56:37 -0500

    adds: Hello world get endpoint

---
+++ src/main/java/edu/san/prep/prepfirstspringproj/HelloController.java
@@ -0,0 +1,14 @@
+package edu.san.prep.prepfirstspringproj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/hello")
+public class HelloController {
+
+  @GetMapping
+  public String hello() {
+    return "Hello World!";
+  }
+
+}
