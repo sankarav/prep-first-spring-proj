1f397b8b5b4ee47e606e54760e3786074b302ac4
Sankar D <san.gct@gmail.com>
2026-02-28 14:49:52 -0500

    init commit

---
+++ src/main/java/edu/san/prep/prepfirstspringproj/PrepFirstSpringProjApplication.java
@@ -0,0 +1,13 @@
+package edu.san.prep.prepfirstspringproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepFirstSpringProjApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrepFirstSpringProjApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/san/prep/prepfirstspringproj/PrepFirstSpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.san.prep.prepfirstspringproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepFirstSpringProjApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
