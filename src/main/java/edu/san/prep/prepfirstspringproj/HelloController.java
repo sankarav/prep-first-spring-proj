26d00c186a2310765900051936eb1ef8e02593db
Sankar D <san.gct@gmail.com>
2026-02-28 15:07:44 -0500

    adds: /api/hello restcontroller and GET endpoint

--- src/main/java/edu/san/prep/prepfirstspringproj/HelloController.java
+++ src/main/java/edu/san/prep/prepfirstspringproj/HelloController.java
@@ -1,14 +1,16 @@
 package edu.san.prep.prepfirstspringproj;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/hello")
+@RestController
+@RequestMapping("/api/hello")
 public class HelloController {
 
   @GetMapping
   public String hello() {
-    return "Hello World!";
+    return "Hello, Spring!";
   }
 
 }
